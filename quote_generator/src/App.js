bd93bec60950fa61272c9e041286153401720be7
Christian Zenaty <cazenaty@yahoo.com>
2017-08-31 16:49:27 -0700

    working twitter button and style update

--- quote_generator/src/App.js
+++ quote_generator/src/App.js
@@ -1,12 +1,61 @@
 import React, {Component} from 'react';
 import './App.css';
 import Generator from './components/quote_generator.js';
+import Twitter from './components/twitter_button.js';
+
+const quotes = [
+  {
+      quote: `“If you cannot do great things, do small things in a great way.”`,
+      author: "Napoleon Hill"
+  }, {
+      quote: `“Growth is painful. Change is painful. But nothing is as painful as staying stuck somewhere you don't belong.”`,
+      author: "Mandy Hale"
+  },
+  {
+      quote: `"The best way to predict the future is to create it.”`,
+      author:'Peter Drucker'
+  },
+  {
+      quote: `“Ask how something can be done rather than say it can't be done."`,
+      author: 'Bo Bennett'
+  }
+]
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        number: 0
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+}
+  
+handleClick() {
+  const max = quotes.length;
+  const random = () => {
+      return Math.floor(Math.random() * (max - 0)) + 0;
+  }
+  console.log(random());
+
+  this.setState({number: random()})
+}
+  
   render() {
     return (
       <div className="App">
-        <div><Generator /></div>
+        <div>
+          <Generator 
+            quotes={quotes} 
+            number={this.state.number} 
+            handleClick={this.handleClick}
+          />
+          <Twitter 
+            quotes={quotes} 
+            number={this.state.number}
+          />
+        </div>
       </div>
     );
   }
--- quote_generator/src/components/quote_generator.js
+++ quote_generator/src/components/quote_generator.js
@@ -1,57 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-const quotes = [
-    {
-        quote: `“If you cannot do great things, do small things in a great way.”`,
-        author: "Napoleon Hill"
-    }, {
-        quote: `“Growth is painful. Change is painful. But nothing is as painful as staying stuck somewhere you don't belong.”`,
-        author: "Mandy Hale"
-    },
-    {
-        quote: `"The best way to predict the future is to create it.”`,
-        author:'Peter Drucker'
-    },
-    {
-        quote: `“Ask how something can be done rather than say it can't be done."`,
-        author: 'Bo Bennett'
-    }
-]
+const Generator = (props) => {
 
-class Generator extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            number: 0
-        };
-
-        this.handleClick = this
-            .handleClick
-            .bind(this);
-    }
-
-    handleClick() {
-        const max = quotes.length;
-        const random = () => {
-            return Math.floor(Math.random() * (max - 0)) + 0;
-        }
-        console.log(random());
-
-        this.setState({number: random()})
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="display">
-                    <p className="quote">{quotes[this.state.number].quote}</p>
-                    <p className="author">~ {quotes[this.state.number].author}</p>
-                </div>
-                <button type="submit" className="button" onClick={this.handleClick}>Next!</button>
+    return (
+        <div>
+            <div className="display">
+                <p className="quote">{props.quotes[props.number].quote}</p>
+                <p className="author">~ {props.quotes[props.number].author}</p>
             </div>
-        )
-    }
+            <button className="button" onClick={props.handleClick}>Next!</button>
+        </div>
+    )
 }
 
 export default Generator;
