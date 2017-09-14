a081d226a5ffeba34bc1701871eb0cbc2ea9e79f
Christian Zenaty <cazenaty@yahoo.com>
2017-09-13 17:07:43 -0700

    added new photos and src's

--- photosite/src/Assets/photos.js
+++ photosite/src/Assets/photos.js
@@ -1,28 +1,64 @@
 /* Construction of AWS s3 bucket via https://learnetto.com/blog/tutorial-how-to-use-amazon-s3-and-cloudfront-cdn-to-serve-images-fast-and-cheap */
 const Photos = [
-    {
+    {   
+        index: 1,
         title: 'Pic 1',
-        src: 'https://d1ch7g5367xvmf.cloudfront.net/Test+Pics/DSCF1867.jpg'
+        src: 'https://s3-us-west-1.amazonaws.com/czphotoassets/assets_live/20160405-DSCF0008.jpg'
     },
-    {
+    {   
+        index: 2,
         title: 'Pic 2',
-        src: 'https://d1ch7g5367xvmf.cloudfront.net/Test+Pics/DSCF1870.jpg'
+        src: 'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160409-DSCF0143.jpg'
     },
     {
+        index: 3,
         title: 'Pic 3',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/Test+Pics/DSCF1872.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160409-DSCF0145.jpg'
     },
     {
+        index: 4,
         title: 'Pic 4',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/Test+Pics/DSCF1885.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160409-DSCF0148.jpg'
     },
-    {
+    {   
+        index: 5,
         title: 'Pic 5',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/Test+Pics/DSCF1900.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160413-DSCF0253.jpg'
     },
     {
+        index: 6,
         title: 'Pic 6',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/Test+Pics/DSCF2191.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160413-DSCF0254.jpg'
+    },
+    {
+        index: 7,
+        title: 'Pic 7',
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160421-DSCF0512.jpg'
+    },
+    {
+        index: 8,
+        title: 'Pic 8',
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160421-DSCF0516.jpg'
+    },
+    {
+        index: 9,
+        title: 'Pic 9',
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160421-DSCF0531.jpg'
+    },
+    {
+        index: 10,
+        title: 'Pic 10',
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160426-DSCF0693.jpg'
+    },
+    {
+        index: 11,
+        title: 'Pic 11',
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160427-DSCF0749.jpg'
+    },
+    {
+        index: 12,
+        title: 'Pic 12',
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160427-DSCF0752.jpg'
     }
 ];
 
