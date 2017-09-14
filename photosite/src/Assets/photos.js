f3198da942c5912a9c73645c1fea448ef87eb004
Christian Zenaty <cazenaty@yahoo.com>
2017-09-14 15:20:17 -0700

    updated src links

--- photosite/src/Assets/photos.js
+++ photosite/src/Assets/photos.js
@@ -3,62 +3,62 @@ const Photos = [
     {   
         index: 1,
         title: 'Pic 1',
-        src: 'https://s3-us-west-1.amazonaws.com/czphotoassets/assets_live/20160405-DSCF0008.jpg'
+        src: 'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0008-1024w.jpg'
     },
     {   
         index: 2,
         title: 'Pic 2',
-        src: 'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160409-DSCF0143.jpg'
+        src: 'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0143-1024w.jpg'
     },
     {
         index: 3,
         title: 'Pic 3',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160409-DSCF0145.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0145-1024w.jpg'
     },
     {
         index: 4,
         title: 'Pic 4',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160409-DSCF0148.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0148-1024w.jpg'
     },
     {   
         index: 5,
         title: 'Pic 5',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160413-DSCF0253.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0253-1024w.jpg'
     },
     {
         index: 6,
         title: 'Pic 6',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160413-DSCF0254.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0254-1024w.jpg'
     },
     {
         index: 7,
         title: 'Pic 7',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160421-DSCF0512.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF05120-683w.jpg'
     },
     {
         index: 8,
         title: 'Pic 8',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160421-DSCF0516.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0516-1024w.jpg'
     },
     {
         index: 9,
         title: 'Pic 9',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160421-DSCF0531.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0531-1024w.jpg'
     },
     {
         index: 10,
         title: 'Pic 10',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160426-DSCF0693.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0693-731w.jpg'
     },
     {
         index: 11,
         title: 'Pic 11',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160427-DSCF0749.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0749-683w.jpg'
     },
     {
         index: 12,
         title: 'Pic 12',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/20160427-DSCF0752.jpg'
+        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0752-1024w.jpg'
     }
 ];
 
