5a9bddac2b06d015dbf91e3ef9d065a8b2782df4
Christian Zenaty <cazenaty@yahoo.com>
2017-09-17 14:00:06 -0700

    improved API data call to GDAX

--- bitviz/src/Components/market_chart.js
+++ bitviz/src/Components/market_chart.js
@@ -5,49 +5,93 @@ var Client = require("coinbase").Client;
 var client = new Client({
   apiKey: "API KEY",
   apiSecret: "API SECRET",
-  strictSSL: false,
+  strictSSL: false
 });
 
 class MarketChart extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.state = {
+      marketOpen: '',
+      currentDateTime: ''
+    };
   }
 
   componentDidMount = () => {
-    this.getData();
+    this.setTime();
+  };
+
+  setTime = () => {
+    /*API requires UTC time */
+    const date = new Date();
+    const currentTime = new Date(date);
+    const currentTimeIso = currentTime.toISOString();
+    const pad = (num) => (num < 10) ? '0' + num: num;
+
+    const year = date.getUTCFullYear();
+    const month = pad(date.getUTCMonth()+1);
+    const day = pad(date.getUTCDate());
+    
+    console.log(
+      "Current Time:",
+      currentTime, typeof currentTime,
+      '\nCurrentIso',
+      currentTimeIso, typeof currentTimeIso
+    );
+
+    const marketOpenDate = `${year}-${month}-${day}T00:00:00Z`;
+    const currentUTCDate = currentTimeIso
+    
+    console.log('currentUTCDate',currentUTCDate, typeof currentUTCDate);
+    console.log('previousUTCDate',marketOpenDate, typeof marketOpenDate);
+
+    this.setState({
+        marketOpen: marketOpenDate,
+        currentDateTime: currentUTCDate
+    });
+      setTimeout(this.getData, 3000)  
   };
 
   getData = () => {
     const BFX_URL = "https://api.coinbase.com/v2/prices/spot?currency=USD";
+    const GDAX_Endpoint = "https://api.gdax.com";
 
-    client.getSpotPrice({ currencyPair: "BTC-USD" }, function(err, price) {
-      err ? console.log("ERROR:", err) : console.log(price);
-    });
+    // client.getSpotPrice({ currencyPair: "BTC-USD" }, function(err, price) {
+    //   err ? console.log("ERROR:", err) : console.log(price);
+    // });
 
-    client.getCurrencies(
-      (err, currencies) =>
-        err ? console.log("Error", err) : console.log(currencies)
-    );
+    // client.getCurrencies(
+    //   (err, currencies) =>
+    //     err ? console.log("Error", err) : console.log(currencies)
+    // );
 
-    Axios.get(BFX_URL)
-      .then(response => console.log(response))
+    // Axios.get(BFX_URL)
+    //   .then(response => console.log(response))
+    //   .catch(err => console.log("ERROR: ", err));
+
+    // Axios.get("/products", {
+    //   baseURL: GDAX_Endpoint
+    // })
+    //   .then(response => console.log("Product List: ", response))
+    //   .catch(err => console.log("ERROR: ", err));
+
+    Axios.get("/products/BTC-USD/candles", {
+      baseURL: GDAX_Endpoint,
+      params: {
+        start: this.state.marketOpen,//"2017-09-17T00:00:00Z",
+        end:  this.state.currentDateTime,//"2017-09-17T20:03:28Z",
+        granularity: "600"
+      }
+    })
+      .then(response => console.log("Historic Rates: ", response))
       .catch(err => console.log("ERROR: ", err));
-
-    //     Axios({
-    //         method: 'get',
-    //         url:'https://cors-anywhere.herokuapp.com/https://api.kraken.com/0/public/Time',
-    //         withCredentials: false,
-    //     })
-    //     .then((response) => console.log(response))
-    //     .catch((err) => console.log('ERROR: ', err));
   };
 
   render() {
     return (
       <div className="temp-container">
-        <div className="market-chart-item">
-          Some market data/ charts will go here!
-        </div>
+        <div className="market-chart-item" />
+        Market Data Here!
       </div>
     );
   }
