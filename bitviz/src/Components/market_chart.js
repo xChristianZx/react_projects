0823538110656a28e61401da3bca5b037d548bd3
Christian Zenaty <cazenaty@yahoo.com>
2017-09-16 19:28:33 -0700

    fixed coinbase API call

--- bitviz/src/Components/market_chart.js
+++ bitviz/src/Components/market_chart.js
@@ -1,51 +1,56 @@
-import React, {Component} from 'react';
-import Axios from 'axios';
-var Client = require('coinbase').Client;
-var client = new Client({'apiKey': 'API KEY',
-'apiSecret': 'API SECRET'});
+import React, { Component } from "react";
+import Axios from "axios";
+
+var Client = require("coinbase").Client;
+var client = new Client({
+  apiKey: "API KEY",
+  apiSecret: "API SECRET",
+  strictSSL: false,
+});
 
 class MarketChart extends Component {
-    constructor(props) {
-        super(props) 
-        
-    }
-    
-    componentDidMount = () => {
-        this.getData();
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    getData = () => {
-        const BFX_URL = 'https://api.coinbase.com/v2/prices/spot?currency=USD';
-        
-        // client.getSpotPrice({'currencyPair': 'BTC-USD'}, function(err, price) {
-        //     console.log(price);
-        //   });
-        
-        // client.getCurrencies((err, currencies) => console.log(currencies))
-        Axios.get(BFX_URL)
-        .then((response) => console.log(response))
-        .catch((err) => console.log('ERROR: ', err));
-        
-        Axios({
-            method: 'get',
-            url:'https://cors-anywhere.herokuapp.com/https://api.kraken.com/0/public/Time',
-            withCredentials: false,
-        })
-        .then((response) => console.log(response))
-        .catch((err) => console.log('ERROR: ', err));
+  componentDidMount = () => {
+    this.getData();
+  };
 
-    }
+  getData = () => {
+    const BFX_URL = "https://api.coinbase.com/v2/prices/spot?currency=USD";
 
-    render () {
-        return (
-            <div className="temp-container">
-                <div className="market-chart-item">
-                    Some market data/ charts will go here!
-                </div>
-            </div>
-        )
-    }
+    client.getSpotPrice({ currencyPair: "BTC-USD" }, function(err, price) {
+      err ? console.log("ERROR:", err) : console.log(price);
+    });
 
+    client.getCurrencies(
+      (err, currencies) =>
+        err ? console.log("Error", err) : console.log(currencies)
+    );
+
+    Axios.get(BFX_URL)
+      .then(response => console.log(response))
+      .catch(err => console.log("ERROR: ", err));
+
+    //     Axios({
+    //         method: 'get',
+    //         url:'https://cors-anywhere.herokuapp.com/https://api.kraken.com/0/public/Time',
+    //         withCredentials: false,
+    //     })
+    //     .then((response) => console.log(response))
+    //     .catch((err) => console.log('ERROR: ', err));
+  };
+
+  render() {
+    return (
+      <div className="temp-container">
+        <div className="market-chart-item">
+          Some market data/ charts will go here!
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MarketChart;
