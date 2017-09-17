a5aa696d1723ea69eb0ddf73a7760cd70b9763e6
Christian Zenaty <cazenaty@yahoo.com>
2017-09-17 16:09:13 -0700

    delete gallery.js

--- photosite/src/App.js
+++ photosite/src/App.js
@@ -4,7 +4,6 @@ import Headline from './Components/headline.js';
 import PhotoList from './Components/photo_list.js';
 import Photos from './Assets/photos.js';
 import LightboxDisplay from './Components/lightbox.js';
-import Gallery from './Components/gallery.js';
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +12,6 @@ class App extends Component {
     this.state = {
       photos: [],
     }
-    // console.log(typeof Photos);
   }
 
   componentDidMount = () => {
@@ -28,7 +26,6 @@ class App extends Component {
         <Headline />
         <LightboxDisplay images={this.state.photos} />
         {/* <PhotoList photos={this.state.photos} /> */}
-        {/* <Gallery images={this.state.photos}/> */}
       </div>
     );
   }
