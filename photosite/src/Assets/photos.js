4dffb77ae69a9adc5d3958d4715a9bee74570b1f
Christian Zenaty <cazenaty@yahoo.com>
2017-09-18 22:37:58 -0700

    rearranged img order

--- photosite/src/Assets/photos.js
+++ photosite/src/Assets/photos.js
@@ -1,65 +1,65 @@
 /* Construction of AWS s3 bucket via https://learnetto.com/blog/tutorial-how-to-use-amazon-s3-and-cloudfront-cdn-to-serve-images-fast-and-cheap */
 const Photos = [
-    {   
-        index: 1,
-        title: 'Pic 1',
-        src: 'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0008-1024w.jpg'
-    },
-    {   
-        index: 2,
-        title: 'Pic 2',
-        src: 'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0143-1024w.jpg'
-    },
-    {
-        index: 3,
-        title: 'Pic 3',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0145-1024w.jpg'
-    },
-    {
-        index: 4,
-        title: 'Pic 4',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0148-1024w.jpg'
-    },
-    {   
-        index: 5,
-        title: 'Pic 5',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0253-1024w.jpg'
-    },
-    {
-        index: 6,
-        title: 'Pic 6',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0254-1024w.jpg'
-    },
-    {
-        index: 7,
-        title: 'Pic 7',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF05120-683w.jpg'
-    },
-    {
-        index: 8,
-        title: 'Pic 8',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0516-1024w.jpg'
-    },
-    {
-        index: 9,
-        title: 'Pic 9',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0531-1024w.jpg'
-    },
-    {
-        index: 10,
-        title: 'Pic 10',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0693-731w.jpg'
-    },
-    {
-        index: 11,
-        title: 'Pic 11',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0749-683w.jpg'
-    },
-    {
-        index: 12,
-        title: 'Pic 12',
-        src:'https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0752-1024w.jpg'
-    }
+  {
+    index: 1,
+    title: "Pic 1",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0008-1024w.jpg"
+  },
+  {
+    index: 2,
+    title: "Pic 2",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0143-1024w.jpg"
+  },
+  {
+    index: 3,
+    title: "Pic 3",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0145-1024w.jpg"
+  },
+  {
+    index: 4,
+    title: "Pic 4",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0148-1024w.jpg"
+  },
+  {
+    index: 5,
+    title: "Pic 5",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0253-1024w.jpg"
+  },
+  {
+    index: 6,
+    title: "Pic 6",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0254-1024w.jpg"
+  },
+  {
+    index: 7,
+    title: "Pic 7",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0752-1024w.jpg"
+  },
+  {
+    index: 8,
+    title: "Pic 8",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0516-1024w.jpg"
+  },
+  {
+    index: 9,
+    title: "Pic 9",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0531-1024w.jpg"
+  },
+  {
+    index: 10,
+    title: "Pic 10",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF05120-683w.jpg"
+  },
+  {
+    index: 11,
+    title: "Pic 11",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0749-683w.jpg"
+  },
+  {
+    index: 12,
+    title: "Pic 12",
+    src: "https://d1ch7g5367xvmf.cloudfront.net/assets_live/DSCF0693-731w.jpg"
+  }
 ];
 
 export default Photos;
