eff9bec95aabef27c05a311ae09c007047f178d4
Christian Zenaty <cazenaty@yahoo.com>
2017-09-25 19:10:00 -0700

    updated styles

--- camper_leaderboard/src/Components/board_list.js
+++ camper_leaderboard/src/Components/board_list.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import BoardListItem from './board_list_item.js';
+import React from "react";
+import BoardListItem from "./board_list_item.js";
 
-const BoardList = ({props, handleAllTimeClick, handleRecentClick}) => {
-    const boardItems = props.map((person, i) => {
-        return <BoardListItem person={person} key={person.username} rank={i} />;
-      });
-      return (
-        <table className="leaderboard-list">
-          <thead>
-            <tr className="board-item-header">
-              <th className="board-item-rank">Rank</th>
-              <th className="board-item-name">Name</th>
-              <th className="board-item-alltime" onClick={() => handleAllTimeClick()}>Alltime</th>
-              <th className="board-item-recent" onClick={() => handleRecentClick()}>Recent</th>
-            </tr>
-          </thead>
-          <tbody>{boardItems}</tbody>
-        </table>
-      );
+const BoardList = ({ props, handleAllTimeClick, handleRecentClick }) => {
+  const boardItems = props.map((person, i) => {
+    return <BoardListItem person={person} key={person.username} rank={i} />;
+  });
+  return (
+    <table className="leaderboard-table">
+      <thead>
+        <tr className="board-item-header">
+          <th className="board-item-rank">Rank</th>
+          <th className="board-item-image-spacer"></th>
+          <th className="board-item-name-container">Name</th>
+          <th
+            className="board-item-alltime"
+            onClick={() => handleAllTimeClick()}
+          >
+            Alltime
+          </th>
+          <th className="board-item-recent" onClick={() => handleRecentClick()}>
+            Recent
+          </th>
+        </tr>
+      </thead>
+      <tbody>{boardItems}</tbody>
+    </table>
+  );
 };
 
 export default BoardList;
--- camper_leaderboard/src/Components/board_list_item.js
+++ camper_leaderboard/src/Components/board_list_item.js
@@ -4,7 +4,7 @@ const BoardListItem = ({ person, rank }) => {
   return (
     <tr className="board-item">
       <td className="board-item-rank">{rank + 1}</td>
-      <td>
+      <td className="board-item-name-container">
         <img
           className="board-item-img"
           src={person.img}
@@ -12,8 +12,9 @@ const BoardListItem = ({ person, rank }) => {
           width="75px"
           height="100px"
         />
+        <span className="board-item-name">{person.username}</span>
       </td>
-      <td className="board-item-name">{person.username}</td>
+      {/* <td className="board-item-name">{person.username}</td> */}
       <td className="board-item-alltime">{person.alltime}</td>
       <td className="board-item-recent">{person.recent}</td>
     </tr>
