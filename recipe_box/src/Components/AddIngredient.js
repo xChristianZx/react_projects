a9d3a75fd6d8899c9ac8ea0b7891bae10026bc38
Christian Zenaty <cazenaty@yahoo.com>
2017-10-04 12:53:06 -0700

    added modal

--- recipe_box/src/Components/AddIngredient.js
+++ recipe_box/src/Components/AddIngredient.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Container, Header, Form, Button, List } from "semantic-ui-react";
+import {
+  Container,
+  Header,
+  Form,
+  Button,
+  List,
+  Modal
+} from "semantic-ui-react";
 
 class AddIngredient extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
-      ingredient: ""
+      ingredient: "",
+      modalOpen: false
     };
-    // this.deleteItem = this.deleteItem.bind(this);
   }
+  handleOpen = () => this.setState({ modalOpen: true });
+
+  handleClose = () => this.setState({ modalOpen: false });
 
   handleChange = e => {
+    console.log(e.target);
     this.setState({
       ingredient: e.target.value
     });
@@ -34,46 +45,58 @@ class AddIngredient extends Component {
     this.setState(prevState => ({
       items: prevState.items.filter(item => item.id !== id)
     }));
-    console.log(e.target);
   };
 
   renderList = list => {
     const items = list.map(item => {
       return (
         <div key={item.id}>
-          <List.Item as='li' className="ingredient-list-item" floated="left">
-            {item.ingredient}            
+          <List.Item as="li" className="ingredient-list-item" floated="left">
+            {item.ingredient}
             <Button
               circular
               size="mini"
               icon="close"
               floated="right"
-              onClick={this.deleteItem.bind(this,item.id)}
+              onClick={this.deleteItem.bind(this, item.id)}
             />
           </List.Item>
         </div>
       );
     });
-    return <List as='ul'>{items}</List>;
+    return <List as="ul">{items}</List>;
   };
 
   render() {
-    console.log(this.state.items);
     return (
-      <Container className="add-ingredients-list-container" text>
-        <Header content="Ingredients" as="h2" textAlign="center" dividing />
-        {this.renderList(this.state.items)}
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Input
-              value={this.state.ingredient}
-              onChange={this.handleChange}
-              width={8}
-            />
-            <Button icon="add" circular />
-          </Form.Group>
-        </Form>
-      </Container>
+      <Modal
+        dimmer={true}
+        trigger={<Button onClick={this.handleOpen}>Add Ingredients</Button>}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        basic
+        size="large"
+      >
+        <Container className="add-ingredients-list-container" text>
+          <Modal.Content>
+            <Header content="Ingredients" as="h2" textAlign="center" dividing />
+            {this.renderList(this.state.items)}
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group>
+                <Form.Input
+                  value={this.state.ingredient}
+                  onChange={this.handleChange}
+                  width={8}
+                />
+                <Button icon="add" circular />
+              </Form.Group>
+            </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button onClick={this.handleClose} floated="right" attached='bottom'>Done</Button>
+          </Modal.Actions>
+        </Container>
+      </Modal>
     );
   }
 }
