4a57fd44d02c5b9dcf5f5d739e274064894143a5
Christian Zenaty <cazenaty@yahoo.com>
2017-10-06 12:54:38 -0700

    full text output

--- recipe_box/src/Components/TextOutput.js
+++ recipe_box/src/Components/TextOutput.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { Accordion } from "semantic-ui-react";
+import { Accordion, List, Message } from "semantic-ui-react";
 // import { Container, Accordion, Icon, Label, List } from "semantic-ui-react";
 
 const TextOutput = ({ recipeList }) => {
   if (!recipeList) {
     return;
   }
-//   console.log(recipeList);
-
   const panels = recipeList.map((panel, i) => {
+    const ingredients = panel.recipe.ingredients;
+    const ingredientList = ingredients.map(item => item.ingredient);
+    const instructions = panel.recipe.instructions;
+
+    console.log("HERE I AM:", ingredientList);
     return {
       title: {
         content: panel.recipe.title,
         key: `title-${i}`
       },
       content: {
-        content: `${panel.recipe.ingredients[i].ingredient}
-        ${panel.recipe.instructions}`,
+        content: (
+          <div>
+            <List items={ingredientList} bulleted />
+            <Message content={instructions} />
+          </div>
+        ),
         key: `content-${i}`
-      }
-
-      // <List items={panel.recipe.ingredients} bulleted/>
+      }      
     };
   });
 
