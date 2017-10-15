9f5c71b2fe696b325e6af9d3d7aeddd0fa27371e
Christian Zenaty <cazenaty@yahoo.com>
2017-10-15 13:33:59 -0700

    updated VChart with righthand Y axis config

--- bitviz/src/Components/MarketOverview/Market_Chart.js
+++ bitviz/src/Components/MarketOverview/Market_Chart.js
@@ -16,16 +16,16 @@ class MarketChart extends Component {
   componentDidMount = () => {
     this.setTime();
   };
-  
+
   setTime = () => {
-    /*GDAX API requires UTC time */
+    /*GDAX API requires UTC time in ISO */
     const date = new Date();
     const currentTime = new Date(date);
     const currentTimeIso = currentTime.toISOString();
-    /**This removes the sec & ms off of the ISOstring so that GDAX returns the timeframe request as requested */
-    const currentUTCDate = currentTimeIso.split('').slice(0,16).join('');
+    /*This removes the sec & ms off of the ISOstring so that GDAX returns the timeframe request as requested */
+    const currentUTCDate = currentTimeIso.split("").slice(0, 16).join("");
     const reformatUTCDate = `${currentUTCDate}Z`;
-    
+
     const pad = num => (num < 10 ? "0" + num : num);
     const year = date.getUTCFullYear();
     const month = pad(date.getUTCMonth() + 1);
@@ -33,31 +33,33 @@ class MarketChart extends Component {
 
     const marketOpenDate = `${year}-${month}-${day}T00:00:00.000Z`;
 
-    console.log("Current Time: ", currentTime, "\nCurrentIso: ", currentTimeIso);
+    console.log("Current Time: ", currentTime, "\nCurrentIso: ",currentTimeIso);
     console.log("MarketOpenUTCDate: ", marketOpenDate, typeof marketOpenDate);
     console.log("currentUTCDate:    ", reformatUTCDate, typeof currentUTCDate);
 
-    this.setState({
-      marketOpen: marketOpenDate,
-      currentDateTime: reformatUTCDate
-    }, () => this.getData());    
+    this.setState(
+      {
+        marketOpen: marketOpenDate,
+        currentDateTime: reformatUTCDate
+      },
+      () => this.getData()
+    );
   };
 
   getData = () => {
-    
     /* https://docs.gdax.com/?javascript#get-historic-rates */
     /*
       RESPONSE FORMAT - [time,low, high, open, close, volume]
     */
-    
+
     const GDAX_Endpoint = "https://api.gdax.com";
-    
+
     Axios.get("/products/BTC-USD/candles", {
       baseURL: GDAX_Endpoint,
       params: {
-        start: this.state.marketOpen ,   /* "2017-09-17T00:00:00Z" */
-        end: this.state.currentDateTime, /* "2017-09-17T20:03:28Z" */
-        granularity: '900', /* 60sec * (desired timeframe in minutes) */
+        start: this.state.marketOpen /* "2017-09-17T00:00:00Z" */,
+        end: this.state.currentDateTime /* "2017-09-17T20:03:28Z" */,
+        granularity: "900" /* 60sec * (desired timeframe in minutes) */
       }
     })
       .then(response => {
@@ -69,16 +71,14 @@ class MarketChart extends Component {
       .catch(err => console.log("ERROR: ", err));
   };
 
-  render() {
-    console.log('TEST1: ', this.state.marketOpen);
-    console.log('TEST2: ', this.state.currentDateTime);
-    console.table(this.state.marketData);
+  render() {  
+    // console.table(this.state.marketData);
     console.log("marketData[0]", this.state.marketData[0]);
     return (
       // <div className="temp-container">
-        <div className="market-chart-item">
-          <PieChart data={this.state.marketData}/>
-        </div>
+      <div className="market-chart-item">
+        <PieChart data={this.state.marketData} />
+      </div>
       // </div>
     );
   }
--- bitviz/src/Components/MarketOverview/VCharts.js
+++ bitviz/src/Components/MarketOverview/VCharts.js
@@ -12,37 +12,38 @@ class PieChart extends Component {
     [time,low, high, open, close, volume]
     */
   render() {
-    /*Converts Epoch unix time to local time */
+    /*Converts Epoch unix time to local time and then creates an OHLC object for VCharts*/
     const reformatData = this.props.data.map(item => {
-      const timeConvert = new Date(
-        item[0] * 1000
-      ).toLocaleTimeString("en-US", {
+      const timeConvert = new Date(item[0] * 1000).toLocaleTimeString("en-US", {
         hour: "numeric",
         minute: "numeric"
       });
-      return {x: timeConvert, open: item[3], close: item[4], high: item[2], low: item[1]};
+      return {
+        x: timeConvert,
+        open: item[3],
+        close: item[4],
+        high: item[2],
+        low: item[1]
+      };
     });
-    console.log('reformatData: ',reformatData);
+
+    const reformatSlice = reformatData.slice(0, 29);
+    console.log("reformatSlice: ", reformatSlice);
 
     return (
       <VictoryChart
         theme={VictoryTheme.material}
-        domain={{ y: [0, 6000]}}
-        domainPadding={{ x: 25 }}
+        padding={50}
+        domainPadding={{ x: [25,50], y: 50 }}        
         scale={{ x: "time" }}
+        height={800}
+        width={1200}        
       >
-        <VictoryAxis         
-        tickFormat={t => `${t.getDate()}/${t.getMonth()}`} />
-        <VictoryAxis dependentAxis />
+        <VictoryAxis fixLabelOverlap={true}/>
+        <VictoryAxis orientation="right" offsetX={50} dependentAxis />
         <VictoryCandlestick
           candleColors={{ positive: "#5f5c5b", negative: "#c43a31" }}
-          data={[
-            { x: new Date(2016, 6, 1), open: 5, close: 10, high: 15, low: 0 },
-            { x: new Date(2016, 6, 2), open: 10, close: 15, high: 20, low: 5 },
-            { x: new Date(2016, 6, 3), open: 15, close: 20, high: 22, low: 10 },
-            { x: new Date(2016, 6, 4), open: 20, close: 10, high: 25, low: 7 },
-            { x: new Date(2016, 6, 5), open: 10, close: 8, high: 15, low: 5 }
-          ]}
+          data={reformatSlice}          
         />
       </VictoryChart>
     );
@@ -50,3 +51,11 @@ class PieChart extends Component {
 }
 
 export default PieChart;
+
+// [
+//   { x: new Date(2016, 6, 1), open: 5, close: 10, high: 15, low: 0 },
+//   { x: new Date(2016, 6, 2), open: 10, close: 15, high: 20, low: 5 },
+//   { x: new Date(2016, 6, 3), open: 15, close: 20, high: 22, low: 10 },
+//   { x: new Date(2016, 6, 4), open: 20, close: 10, high: 25, low: 7 },
+//   { x: new Date(2016, 6, 5), open: 10, close: 8, high: 15, low: 5 }
+// ]
