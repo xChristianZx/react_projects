65073fde559ab4339275cb5b099c726203aef969
Christian Zenaty <cazenaty@yahoo.com>
2017-10-19 15:13:07 -0700

    added footer

--- bitviz/src/App.js
+++ bitviz/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import MarketChart from './Components/MarketOverview/Market_Chart';
+import Footer from './Components/Footer/Footer';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Header />
         <Navbar />
         <MarketChart />
+        <Footer className="footer"/>
       </div>
     );
   }
---
+++ bitviz/src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-disclaimer">
+        <p>Note: All prices quoted from GDAX exchange</p>
+      </div>
+      <div className="footer-name">
+        <p>A CZ production</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
