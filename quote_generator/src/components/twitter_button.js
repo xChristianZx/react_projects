b9b195d55d39d0930f7b65c11873252532867e4c
Christian Zenaty <cazenaty@yahoo.com>
2017-11-01 16:59:48 -0700

    prettified

--- quote_generator/src/components/twitter_button.js
+++ quote_generator/src/components/twitter_button.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Twitter = (props) => {
-    const quote = props.quotes[props.number].quote;
-    const author = props.quotes[props.number].author;
-    const currentQuote = `${quote} ~ ${author}`;
+const Twitter = props => {
+  const quote = props.quotes[props.number].quote;
+  const author = props.quotes[props.number].author;
+  const currentQuote = `${quote} ~ ${author}`;
 
-    function createTweet (quotes, author) {
-        window.open("https://twitter.com/intent/tweet?&text=" + encodeURIComponent(currentQuote));
-    }
+  function createTweet(quotes, author) {
+    window.open(
+      "https://twitter.com/intent/tweet?&text=" +
+        encodeURIComponent(currentQuote)
+    );
+  }
 
-    return (
-        <div>
-            <button className="twitter-button" onClick={createTweet}>Tweet it!</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button className="twitter-button" onClick={createTweet}>
+        Tweet it!
+      </button>
+    </div>
+  );
+};
 
 export default Twitter;
