460f07de5b862caee257a5086c4dc95af17664bf
Christian Zenaty <cazenaty@yahoo.com>
2017-11-01 16:58:54 -0700

    prettified

--- quote_generator/src/components/quote_generator.js
+++ quote_generator/src/components/quote_generator.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Generator = (props) => {
-
-    return (
-        <div>
-            <div className="display">
-                <p className="quote">{props.quotes[props.number].quote}</p>
-                <p className="author">~ {props.quotes[props.number].author}</p>
-            </div>
-            <button className="button" onClick={props.handleClick}>Next!</button>
-        </div>
-    )
-}
+const Generator = props => {
+  return (
+    <div>
+      <div className="display">
+        <p className="quote">{props.quotes[props.number].quote}</p>
+        <p className="author">~ {props.quotes[props.number].author}</p>
+      </div>
+      <button className="button" onClick={props.handleClick}>
+        Next!
+      </button>
+    </div>
+  );
+};
 
 export default Generator;
