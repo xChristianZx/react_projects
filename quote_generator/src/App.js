7e556a4a484c16d10e0208f0f20458000630c6fb
Christian Zenaty <cazenaty@yahoo.com>
2017-11-01 17:07:43 -0700

    refactor and seperation of quote list

--- quote_generator/src/App.js
+++ quote_generator/src/App.js
@@ -1,26 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
+import quotes from "./components/quote_list";
 import Generator from "./components/quote_generator.js";
 import Twitter from "./components/twitter_button.js";
-
-const quotes = [
-  {
-    quote: `“If you cannot do great things, do small things in a great way.”`,
-    author: "Napoleon Hill"
-  },
-  {
-    quote: `“Growth is painful. Change is painful. But nothing is as painful as staying stuck somewhere you don't belong.”`,
-    author: "Mandy Hale"
-  },
-  {
-    quote: `"The best way to predict the future is to create it.”`,
-    author: "Peter Drucker"
-  },
-  {
-    quote: `“Ask how something can be done rather than say it can't be done."`,
-    author: "Bo Bennett"
-  }
-];
 
 class App extends Component {
   constructor(props) {
@@ -29,19 +11,15 @@ class App extends Component {
     this.state = {
       number: 0
     };
-
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
+  handleClick = () => {
     const max = quotes.length;
     const random = () => {
       return Math.floor(Math.random() * (max - 0)) + 0;
     };
-    console.log(random());
-
     this.setState({ number: random() });
-  }
+  };
 
   render() {
     return (
